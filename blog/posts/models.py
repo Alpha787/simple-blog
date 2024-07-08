a80b3b70c38fc53bab96fff7dafc8e4187b71783
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-07-08 08:20:11 +0300

    Added models Post, Comment

---
+++ blog/posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  text = models.TextField()
+  created_date = models.DateTimeField()
+  published_date = models.DateTimeField()
+
+class Comment(models.Model):
+  text = models.TextField()
+  published_date = models.DateTimeField()
