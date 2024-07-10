a2bce3bdf3a72f9c2b25bbba0f9db8c4279c4d1c
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-07-10 11:14:29 +0300

    Added path for post, user

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from posts.views import post_list
+from users.views import users_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('post', post_list, name='posts_data'),
+    path('user', users_list, name='users_data'),
 ]
