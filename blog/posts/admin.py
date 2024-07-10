7c3074396984c88c68a79292ade4991f7cb65346
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-07-10 11:14:53 +0300

    Added model register

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from posts.models import Post
+from django import forms
 # Register your models here.
+
+# class PostForm(forms.ModelForm):
+#   class Meta:
+#     model = Post
+#     fields = '__all__'
+
+admin.site.register(Post)
