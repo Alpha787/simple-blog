c9f633cfe97eb1a87377cb9ad197eb364a13dada
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-07-15 23:17:03 +0300

    Added author field

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
   title = models.CharField(max_length=200)
-  text = models.TextField()
+  text = models.TextField(blank=True)
   created_date = models.DateTimeField(auto_created=True)
   published_date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.text
