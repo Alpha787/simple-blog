101276852a9fcbb179fd5b40fe952f6365704e94
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-07-17 21:27:37 +0300

    Deleted unused functions

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,25 +1,96 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.db import connection
-from .models import *
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+# from django.http import HttpResponse
+from django.views.generic import TemplateView, View, DetailView, UpdateView, DeleteView, CreateView
+# from django.db import connection
+# from django.forms import ModelForm
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
-# главная страница блога
-def post_list(request):
-  posts = Post.objects.all()
-  print(posts)
-  print(posts.query)
-  print(connection.queries)
-  return render(request, 'posts.html', {'posts':posts})
+class PostView(View):
+  template_name = "posts.html"
+  model = Post
+  
+  # создание поста
+  def create(self, request, *args, **kwargs):
+    context = {}
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+      form.save()
+      
+    context['form'] = form
+    return render(request, "post_create.html", context)
 
-# страница отдельной статьи
-# def post(request):
-#   pass
+  
+  # получение списка постов
+  def get(self, request, *args, **kwargs):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request, 'posts.html', context)
 
-# # регистрация пользователя
-# def register_page(request):
-#   pass
+  # обновление поста
+  def post(self, request, post_id):
+    post = Post.objects.get(id=post_id)
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+      form.save()
+      return redirect('post_detail', post_id=post_id)
+    else:
+      context = {'form':form, 'post':post}
+      return render(request, 'post_update.html', context)
+  
+  # удаление поста
+  def delete(self, request, *args, **kwargs):
+    pass
 
-# # административная панель
-# def admin_panel():
-#   pass
+class PostCreateView(CreateView):
+  template_name = "post_create.html"
+  model = Post
+
+  def create(self, request, post_id):
+    context = {}
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+      form.save()
+      
+    context['form'] = form
+    return render(request, "post_create.html", context)
+
+# Модель для вывода одной статьи. Работает.
+class PostDetailView(DetailView):
+  template_name = "post_detail.html"
+  model = Post
+  
+  def get(self, request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'post_detail.html', {'post':post})
+
+# Модель для метода Update
+class PostUpdateView(UpdateView):
+  template_name = "post_update.html"
+  model = Post
+
+  def put(self, request, post_id):
+    context = {}
+    obj = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None, instance=obj)
+
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect("/"+id)
+    
+    context["form"] = form
+    return render(request, "post_update.html", context)
+    
+# Модель для метода Delete
+class PostDeleteView(DeleteView):
+  template_name = "post_delete.html"
+  model = Post
+
+  def delete(self, request, post_id):
+    context = {}
+    obj = get_object_or_404(Post, id=post_id)
+
+    if request.method == "POST":
+      obj.delete()
+      return HttpResponseRedirect("/")
+    return render(request, "delete_view.html", context)
