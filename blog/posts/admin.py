7f0b5e21003cb58f93da205faeba3a26b1226a1b
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-07-23 22:06:14 +0300

    Added formfield_overrides

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea, TextInput
+# from django.forms import widgets
+
 from posts.models import Post
+
 # Register your models here.
 
 # class PostForm(forms.ModelForm):
@@ -13,6 +18,9 @@ class PostAdmin(admin.ModelAdmin):
   list_display = ("id", "image","author","title", "text", "created_date", "published_date")
   list_filter = ("author", "created_date", "published_date")
   date_hierarchy = ("published_date")
-  
+  formfield_overrides = {
+    # models.CharField: {'widget': TextInput(attrs={'size':20})},
+    models.TextField: {"widget": Textarea(attrs={"rows":10, "cols":100})},
+  }
 
 admin.site.register(Post, PostAdmin)
