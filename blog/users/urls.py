ac8fef12f5890c782612746ffbd59b2470549d08
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-09-17 14:33:41 +0300

    Added urls for login and register

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from users.views import login_user
+from users.views import login_user, register, login_view
 # from . import views
 
 urlpatterns = [
-    path('login_user', login_user, name='login')
+    # path('login_user', login_user, name='login'),
+    path('login/', login_view, name='login'),
+    path('register/', register, name='register'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
