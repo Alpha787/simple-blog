dea0278e07bc90fbe6ad89efc92549fc05aa1040
Vasil Stepanov <48417218+Alpha787@users.noreply.github.com>
2024-09-17 14:08:36 +0300

    Added captcha urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,14 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include  
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')), # перенаправление на posts
     path('', include('users.urls')), # перенаправление на users
     path('', include('django.contrib.auth.urls')), # перенаправление на страницу логина
+    path('captcha/', include('captcha.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
 
 if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    import debug_toolbar
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
+
+    
